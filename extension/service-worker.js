f432e3695fe1f397ffd594eabcc8c9f3a9e3eabe
loo <luyia@trip.com>
2023-06-08 00:03:54 +0800

    add extension chrome sample

---
+++ extension/service-worker.js
@@ -0,0 +1,27 @@
+const Match_URL = 'ctrip.com';
+
+console.log(`test===>`, Match_URL)
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+    console.log(`tab`, tab, tab.url)
+    if (!tab.url) return;
+    const url = new URL(tab.url);
+    // Enables the side panel on google.com
+    if (url.origin.includes(Match_URL)) {
+        await chrome.sidePanel.setOptions({
+        tabId,
+        path: 'sidepanel.html',
+        enabled: true
+        });
+        console.log(`chrome.sidePanel`)
+        chrome.sidePanel
+        .setPanelBehavior({ openPanelOnActionClick: true })
+        .catch((error) => console.error(error));
+    } else {
+        // Disables the side panel on all other sites
+        await chrome.sidePanel.setOptions({
+        tabId,
+        path: 'sidepanel.html',
+        enabled: false
+        });
+    }
+});
