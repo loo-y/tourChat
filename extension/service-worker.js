22d98ca6fb0add8f6e965515d2cd3e50a489a4e6
loo <luyia@trip.com>
2023-06-11 11:03:08 +0800

    prettier extension code

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,27 +1,25 @@
-const Match_URL = 'ctrip.com';
+const Match_URL = 'ctrip.com'
 
 console.log(`test===>`, Match_URL)
 chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
     console.log(`tab`, tab, tab.url)
-    if (!tab.url) return;
-    const url = new URL(tab.url);
+    if (!tab.url) return
+    const url = new URL(tab.url)
     // Enables the side panel on google.com
     if (url.origin.includes(Match_URL)) {
         await chrome.sidePanel.setOptions({
-        tabId,
-        path: 'sidepanel.html',
-        enabled: true
-        });
+            tabId,
+            path: 'sidepanel.html',
+            enabled: true,
+        })
         console.log(`chrome.sidePanel`)
-        chrome.sidePanel
-        .setPanelBehavior({ openPanelOnActionClick: true })
-        .catch((error) => console.error(error));
+        chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true }).catch(error => console.error(error))
     } else {
         // Disables the side panel on all other sites
         await chrome.sidePanel.setOptions({
-        tabId,
-        path: 'sidepanel.html',
-        enabled: false
-        });
+            tabId,
+            path: 'sidepanel.html',
+            enabled: false,
+        })
     }
-});
+})
