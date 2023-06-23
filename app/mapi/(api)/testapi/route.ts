20cb9c76f0b67fb73f99a10cad27918394214368
loo <luyia@trip.com>
2023-06-23 15:58:07 +0800

    build extension

--- app/mapi/(api)/testapi/route.ts
+++ app/mapi/(api)/testapi/route.ts
@@ -4,6 +4,18 @@ import { modelGPT35Turbo, modelTextEmbeddingAda002 } from '../../azure/common'
 
 export async function GET(request: NextRequest) {
     let response: any = undefined
+    // try {
+    //     response = await modelTextEmbeddingAda002.embedDocuments(['Hello world', 'Bye bye'])
+    //     // response = await modelTextEmbeddingAda002.embedQuery("I enjoy walking with my cute dog.")
+    // } catch (e) {
+    //     console.log(`getEmbeddingsFromOpenai error`, e)
+    // }
+
+    return NextResponse.json({ satus: 0, response }, { status: 200 })
+}
+
+export async function POST(request: NextRequest) {
+    let response: any = undefined
     try {
         response = await modelTextEmbeddingAda002.embedDocuments(['Hello world', 'Bye bye'])
         // response = await modelTextEmbeddingAda002.embedQuery("I enjoy walking with my cute dog.")
