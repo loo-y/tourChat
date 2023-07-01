7c9d389ceb76328f44352dbb80361eef26edffa0
loo <luyia@trip.com>
2023-07-01 20:25:23 +0800

    add a function to split page content by max token limitation

--- app/mapi/(api)/vector-save/route.ts
+++ app/mapi/(api)/vector-save/route.ts
@@ -3,9 +3,12 @@ import { NextRequest, NextResponse } from 'next/server'
 import { sha256_16bit } from '../../util'
 import { getIndex, createIndex, insert, findSimilar, deleteAllVectors } from '../../pinecone/connect'
 import { getEmbeddings as getEmbeddingsAzure } from '../../azure/connect'
+import { inputTokenLimitation } from '../../azure/common'
 import { getEmbeddingsFromHfInference as getEmbeddingsHF } from '../../huggingFace/connnect'
 import { VectorSaveParams } from '../../interface'
 import { openaiPineconeIndex } from '../../pinecone/constants'
+import { GPTTokens } from 'gpt-tokens'
+const tokenCalcModel = `gpt-3.5-turbo`
 
 interface VectorSaveStream extends ReadableStream<Uint8Array>, VectorSaveParams {}
 
@@ -25,6 +28,11 @@ const vectorSave = async (params: VectorSaveParams) => {
         return { satus: -1 }
     }
 
+    const chunkedContextList = chunkContextListByTokenLimitation({
+        contextList,
+        tokenLimitation: inputTokenLimitation.embeddings,
+    })
+    // return {chunkedContextList}
     // delete first
     await deleteAllVectors({ index: openaiPineconeIndex, namespace: sha256_namespace })
 
@@ -67,4 +75,68 @@ const vectorSave = async (params: VectorSaveParams) => {
     )
 
     return { upsertedTotalCount, vectorsTotal, namespace: sha256_namespace }
+}
+
+const chunkContextListByTokenLimitation = ({
+    contextList,
+    tokenLimitation,
+}: Pick<VectorSaveParams, 'contextList'> & {
+    tokenLimitation: number
+}): VectorSaveParams['contextList'] => {
+    if (!(tokenLimitation > 0)) {
+        return contextList
+    }
+    try {
+        let chunkedContextList: VectorSaveParams['contextList'] = []
+        _.map(contextList, (ctx: any) => {
+            const { pageContent, metadata } = ctx || {}
+            if (pageContent) {
+                const pageContentTokenItem = new GPTTokens({
+                    messages: [
+                        {
+                            role: 'system',
+                            content: pageContent,
+                        },
+                    ],
+                    model: tokenCalcModel,
+                })
+                const { usedTokens } = pageContentTokenItem || {}
+                console.log(`usedTokens`, usedTokens)
+                if (usedTokens <= tokenLimitation) {
+                    chunkedContextList.push(ctx)
+                } else {
+                    const chunkedPageContent = _.chunk(
+                        pageContent,
+                        Math.floor(pageContent.length / Math.ceil(usedTokens / tokenLimitation))
+                    )
+                    _.map(chunkedPageContent, (chunkedPageContentItem, index) => {
+                        const splitedPageContent = chunkedPageContentItem.join('')
+                        const splitedPageContentTokenItem = new GPTTokens({
+                            messages: [
+                                {
+                                    role: 'system',
+                                    content: splitedPageContent,
+                                },
+                            ],
+                            model: tokenCalcModel,
+                        })
+                        chunkedContextList.push({
+                            pageContent: splitedPageContent,
+                            metadata: {
+                                ...metadata,
+                                token: splitedPageContentTokenItem?.usedTokens || 0,
+                                chunkIndex: index,
+                                chunkTotal: chunkedPageContent.length,
+                            },
+                        })
+                    })
+                }
+            }
+        })
+        return chunkedContextList
+    } catch (e) {
+        console.log(`chunkContextListByTokenLimitation error`, e)
+    }
+
+    return contextList
 }
